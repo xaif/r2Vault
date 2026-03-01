a3db0428dc342b9ee24798b2c24b525d21cb7cd1
Xaif <huzaifaamirka46@gmail.com>
2026-03-02 00:31:04 +0530

    Initial Commit

---
+++ Fiaxe/FiaxeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiaxeApp.swift
+//  Fiaxe
+//
+//  Created by Xaif on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct FiaxeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
